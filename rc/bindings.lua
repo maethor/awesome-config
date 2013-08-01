c320ca6c430e122268c90903aea9a8722d9032f3
Guillaume Subiron <maethor@subiron.org>
2013-08-01 20:25:00 +0200

    Unable awesome menu on right clic

--- rc/bindings.lua
+++ rc/bindings.lua
@@ -7,7 +7,7 @@ local mpc = loadrc("mpc", "maethor/mpc")
 --------------------
 
 root.buttons(awful.util.table.join(
-    awful.button({ }, 3, function () mymainmenu:toggle() end),
+    --awful.button({ }, 3, function () mymainmenu:toggle() end),
     awful.button({ }, 4, awful.tag.viewnext),
     awful.button({ }, 5, awful.tag.viewprev)
 ))
